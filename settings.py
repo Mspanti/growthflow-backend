4c3ef7294fc95e77d553743a7b7c1735afb74c7a
Soundar_MS <134880830+Mspanti@users.noreply.github.com>
2025-06-27 13:29:02 +0530

    Update settings.py

--- settings.py
+++ settings.py
@@ -86,11 +86,11 @@ WSGI_APPLICATION = 'wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ.get('POSTGRES_DB', 'growthflow_db'),
-        'USER': os.environ.get('POSTGRES_USER', 'postgres'),
-        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'admin2310'),
-        'HOST': os.environ.get('POSTGRES_HOST', 'db'),
-        'PORT': os.environ.get('POSTGRES_PORT', '5432'),
+        'NAME': os.environ.get('POSTGRES_DB'),       
+        'USER': os.environ.get('POSTGRES_USER'),      
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'), 
+        'HOST': os.environ.get('POSTGRES_HOST'),      
+        'PORT': os.environ.get('POSTGRES_PORT'),     
     }
 }
 
